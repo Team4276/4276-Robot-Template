5902f12ca67e77c50a37435bc7e18935df65e3c8
Breadfest <ethanoskatt1@gmail.com>
2026-08-12 15:59:29 -0700

    Update FieldLayout.java

--- src/main/java/frc/robot/game/FieldLayout.java
+++ src/main/java/frc/robot/game/FieldLayout.java
@@ -1,8 +1,23 @@
 package frc.robot.game;
 
-import edu.wpi.first.units.Units;
+import static edu.wpi.first.units.Units.Meters;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.units.measure.Distance;
 
-public class FieldLayout {
-    public static final Distance kFieldWidth = Units.Meter.of(1.0);
+public class FieldLayout {    
+    public static final AprilTagFieldLayout kApriltagLayout = AprilTagFieldLayout
+            .loadField(AprilTagFields.k2026RebuiltWelded);
+
+    public static final Distance kFieldLength = Meters.of(kApriltagLayout.getFieldLength());
+    public static final Distance kFieldWidth = Meters.of(kApriltagLayout.getFieldWidth());
+
+    public static class LinesVertical {
+        public static final Distance kCenter = kFieldLength.div(2.0);
+    }
+
+    public static class LinesHorizontal {
+        public static final Distance kCenter = kFieldWidth.div(2.0);
+    }
 }
